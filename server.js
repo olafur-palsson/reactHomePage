817ad545858b2062ce95cd206afc63c3d7ae5aa3
olafur-palsson <olafur.palsson2@gmail.com>
2018-02-16 15:45:44 +0000

    Server is ready

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const serveStatic = require("serve-static")
 
 $e = Express()
 
-$e.set("port", process.env.PORT || 8080)
+$e.set("port", process.env.PORT || 80)
 
 $e.use(serveStatic(__dirname))
-console.log($e.get("port"))
 $e.listen($e.get("port"))
