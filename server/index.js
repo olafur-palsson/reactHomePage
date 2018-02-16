f20c91940f9fee144ac69a925279ff96e5b7a877
olafur-palsson <olafur.palsson2@gmail.com>
2018-02-16 14:47:25 +0000

    Syntax error fix

--- server/index.js
+++ server/index.js
@@ -3,6 +3,6 @@ const Express = require("express")
 $e = Express()
 
 $e.get("/", (req, res) => res.send("Hello World!"))
-$e.use("./..", express.static("public"))
+$e.use("./..", $e.static("public"))
 
 $e.listen(3000, () => console.log("shiiiimang"))
